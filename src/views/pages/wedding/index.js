70ac1a503366cc2a6b816872a07f143b50b4867c
Huang Wei-Hao <demotascha@gmail.com>
2017-06-14 23:17:41 +0800

    copy task-form/index to wedding-index

--- src/views/pages/wedding/index.js
+++ src/views/pages/wedding/index.js
@@ -1,19 +1,92 @@
-import React from 'react';
+import { List } from 'immutable';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
+import { createSelector } from 'reselect';
+
+import { getNotification, notificationActions } from 'src/core/notification';
+import { getTaskFilter, getVisibleTasks, tasksActions } from 'src/core/tasks';
+import Notification from '../../components/notification';
+import WeddingForm from '../../components/wedding-form';
 
 
-export function Wedding() {
-  return (
-    <div className="g-row sign-in">
-      <div className="g-col">
-        <h1 className="sign-in__heading">Wedding Page</h1>
+export class Tasks extends Component {
+  static propTypes = {
+    createTask: PropTypes.func.isRequired,
+    deleteTask: PropTypes.func.isRequired,
+    dismissNotification: PropTypes.func.isRequired,
+    filterTasks: PropTypes.func.isRequired,
+    filterType: PropTypes.string.isRequired,
+    loadTasks: PropTypes.func.isRequired,
+    location: PropTypes.object.isRequired,
+    notification: PropTypes.object.isRequired,
+    tasks: PropTypes.instanceOf(List).isRequired,
+    undeleteTask: PropTypes.func.isRequired,
+    unloadTasks: PropTypes.func.isRequired,
+    updateTask: PropTypes.func.isRequired
+  };
+
+  componentWillMount() {
+    this.props.loadTasks();
+    this.props.filterTasks(this.props.location.query.filter);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.location.query.filter !== this.props.location.query.filter) {
+      this.props.filterTasks(nextProps.location.query.filter);
+    }
+  }
+
+  componentWillUnmount() {
+    this.props.unloadTasks();
+  }
+
+  renderNotification() {
+    const { notification } = this.props;
+    return (
+      <Notification
+        action={this.props.undeleteTask}
+        actionLabel={notification.actionLabel}
+        dismiss={this.props.dismissNotification}
+        display={notification.display}
+        message={notification.message}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <div className="g-row">
+        <div className="g-col">
+          <WeddingForm createTask={this.props.createTask} />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
 
 //=====================================
 //  CONNECT
 //-------------------------------------
 
-export default connect(null)(Wedding);
+const mapStateToProps = createSelector(
+  getNotification,
+  getTaskFilter,
+  getVisibleTasks,
+  (notification, filterType, tasks) => ({
+    notification,
+    filterType,
+    tasks
+  })
+);
+
+const mapDispatchToProps = Object.assign(
+  {},
+  tasksActions,
+  notificationActions
+);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Tasks);
