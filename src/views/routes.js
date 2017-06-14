8c028f33ccaec4af536b63b14110ffc2d831be9a
Huang Wei-Hao <demotascha@gmail.com>
2017-06-14 10:09:26 +0800

    add wedding/index into views/routes

--- src/views/routes.js
+++ src/views/routes.js
@@ -2,12 +2,14 @@ import { isAuthenticated } from 'src/core/auth';
 import App from './app';
 import SignIn from './pages/sign-in';
 import Tasks from './pages/tasks';
+import Wedding from './pages/wedding';
 
 
 export const paths = {
   ROOT: '/',
   SIGN_IN: '/sign-in',
-  TASKS: '/'
+  TASKS: '/signed',
+  WEDDING: '/wedding'
 };
 
 
@@ -35,14 +37,19 @@ export const getRoutes = getState => {
     childRoutes: [
       {
         indexRoute: {
-          component: Tasks,
-          onEnter: requireAuth(getState)
+          component: Wedding,
+          onEnter: getState
         }
       },
       {
         path: paths.SIGN_IN,
         component: SignIn,
         onEnter: requireUnauth(getState)
+      },
+      {
+        path: paths.TASKS,
+        component: Tasks,
+        onEnter: requireAuth(getState)
       }
     ]
   };
