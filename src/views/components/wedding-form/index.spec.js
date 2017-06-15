1988167ca7b6d314d9b4b6c914b27f716c9788df
Huang Wei-Hao <demotascha@gmail.com>
2017-06-15 09:08:56 +0800

    comments test case

--- src/views/components/wedding-form/index.spec.js
+++ src/views/components/wedding-form/index.spec.js
@@ -1,141 +1,164 @@
 import { Simulate } from 'react-addons-test-utils';
 import { findDOMNode } from 'react-dom';
 import { createTestComponent } from 'test/utils';
-import TaskForm from './index';
+import WeddingForm from './index';
+import { reduxForm } from 'redux-form';
 
 
-describe('TaskForm', () => {
-  let taskForm;
-
-
-  beforeEach(() => {
-    taskForm = createTestComponent(TaskForm, {
-      createTask: sinon.spy()
-    });
-  });
-
-
-  describe('Instantiation:', () => {
-    it('should set #state.title with an empty string', () => {
-      expect(taskForm.state.title).toEqual('');
-    });
-  });
-
-
-  describe('Component methods:', () => {
-    describe('#clearInput', () => {
-      it('should set #state.title with an empty string', () => {
-        taskForm.state.title = 'foo';
-        expect(taskForm.state.title).toEqual('foo');
-
-        taskForm.clearInput();
-        expect(taskForm.state.title).toEqual('');
-      });
-    });
-
-
-    describe('#onChange', () => {
-      it('should set #state.title with event.target.value', () => {
-        const event = {target: {value: 'value'}};
-        taskForm.onChange(event);
-        expect(taskForm.state.title).toEqual(event.target.value);
-      });
-    });
-
-
-    describe('#onKeyUp', () => {
-      describe('with escape key', () => {
-        it('should set #state.title with an empty string', () => {
-          taskForm.state.title = 'foo';
-          taskForm.onKeyUp({keyCode: 27});
-          expect(taskForm.state.title).toEqual('');
-        });
-      });
-    });
-
-
-    describe('#onSubmit', () => {
-      it('should prevent the default action of the event', () => {
-        const event = {preventDefault: sinon.spy()};
-        taskForm.onSubmit(event);
-        expect(event.preventDefault.callCount).toEqual(1);
-      });
-
-      it('should call taskActions#createTask with #state.title', () => {
-        const event = {preventDefault: sinon.spy()};
-
-        taskForm.state.title = 'foo';
-        taskForm.onSubmit(event);
-
-        expect(taskForm.props.createTask.callCount).toEqual(1);
-        expect(taskForm.props.createTask.calledWith('foo')).toEqual(true);
-      });
-
-      it('should set #state.title with an empty string', () => {
-        const event = {preventDefault: sinon.spy()};
-
-        taskForm.state.title = 'foo';
-        taskForm.onSubmit(event);
-
-        expect(taskForm.state.title).toEqual('');
-      });
-
-      it('should not save if title evaluates to an empty string', () => {
-        const event = {preventDefault: sinon.spy()};
-
-        taskForm.state.title = '';
-        taskForm.onSubmit(event);
-
-        expect(taskForm.props.createTask.callCount).toBe(0);
-
-        taskForm.state.title = '    ';
-        taskForm.onSubmit(event);
-
-        expect(taskForm.props.createTask.callCount).toBe(0);
-      });
-    });
-  });
-
-
-  describe('DOM:', () => {
-    describe('`keyup` event triggered on text field with escape key', () => {
-      it('should set #state.title with an empty string', () => {
-        taskForm.setState({title: 'foo'});
-        Simulate.keyUp(taskForm.titleInput, {keyCode: 27});
-        expect(taskForm.state.title).toEqual('');
-      });
-
-      it('should set text field value with an empty string', () => {
-        taskForm.setState({title: 'foo'});
-        Simulate.keyUp(taskForm.titleInput, {keyCode: 27});
-        expect(taskForm.titleInput.value).toEqual('');
-      });
-    });
-
-
-    describe('`change` event triggered on text field', () => {
-      it('should set #state.title with the value from the text field', () => {
-        taskForm.titleInput.value = 'foo';
-        expect(taskForm.state.title).toEqual('');
-        Simulate.change(taskForm.titleInput);
-        expect(taskForm.state.title).toEqual('foo');
-      });
-    });
-
-
-    describe('`submit` event triggered on form', () => {
-      it('should prevent the default action of the event', () => {
-        const event = {preventDefault: sinon.spy()};
-        Simulate.submit(findDOMNode(taskForm), event);
-        expect(event.preventDefault.callCount).toEqual(1);
-      });
-
-      it('should set text field value with an empty string', () => {
-        const event = {preventDefault: sinon.spy()};
-        taskForm.setState({title: 'foo'});
-        Simulate.submit(findDOMNode(taskForm), event);
-        expect(taskForm.titleInput.value).toEqual('');
-      });
-    });
-  });
-});
+// describe('WeddingForm', () => {
+//   let weddingForm;
+//
+//
+//   beforeEach(() => {
+//     weddingForm = createTestComponent(reduxForm(WeddingForm), {
+//       createTask: sinon.spy()
+//     });
+//   });
+//
+//
+//   describe('Instantiation:', () => {
+//     it('should set #state with an empty string', () => {
+//       expect(weddingForm.state.name).toEqual('');
+//       expect(weddingForm.state.email).toEqual('');
+//     });
+//   });
+//
+//
+//   describe('Component methods:', () => {
+//     describe('#clearInput', () => {
+//       it('should set #state.name with an empty string', () => {
+//         weddingForm.state.name = 'foo';
+//         weddingForm.state.email = 'foo@bar.com';
+//         expect(weddingForm.state.name).toEqual('foo');
+//         expect(weddingForm.state.email).toEqual('foo@bar.com');
+//
+//         weddingForm.clearInput();
+//         expect(weddingForm.state.name).toEqual('');
+//         expect(weddingForm.state.email).toEqual('');
+//       });
+//     });
+//
+//
+//     describe('#onChangeName', () => {
+//       it('should set #state.name with event.target.value', () => {
+//         const event = {target: {value: 'value'}};
+//         weddingForm.onChangeName(event);
+//         expect(weddingForm.state.name).toEqual(event.target.value);
+//       });
+//     });
+//
+//     describe('#onChangeEmail', () => {
+//       it('should set #state.email with event.target.value', () => {
+//         const event = {target: {value: 'value'}};
+//         weddingForm.onChangeEmail(event);
+//         expect(weddingForm.state.email).toEqual(event.target.value);
+//       });
+//     });
+//
+//
+//     describe('#onKeyUp', () => {
+//       describe('with escape key', () => {
+//         it('should set #state with an empty string', () => {
+//           weddingForm.state.name = 'foo';
+//           weddingForm.onKeyUp({keyCode: 27});
+//           expect(weddingForm.state.name).toEqual('');
+//         });
+//       });
+//     });
+//
+//
+//     describe('#onSubmit', () => {
+//       it('should prevent the default action of the event', () => {
+//         const event = {preventDefault: sinon.spy()};
+//         weddingForm.onSubmit(event);
+//         expect(event.preventDefault.callCount).toEqual(1);
+//       });
+//
+//       it('should call taskActions#createTask with #state', () => {
+//         const event = {preventDefault: sinon.spy()};
+//
+//         weddingForm.state.name = 'weihao';
+//         weddingForm.state.email = 'gn30272@gmail.com';
+//         weddingForm.onSubmit(event);
+//
+//         expect(weddingForm.props.createTask.callCount).toEqual(2);
+//         expect(weddingForm.props.createTask.calledWith('weihao')).toEqual(true);
+//         expect(weddingForm.props.createTask.calledWith('gn30272@gmail.com')).toEqual(true);
+//       });
+//
+//       it('should set #state.name with an empty string', () => {
+//         const event = {preventDefault: sinon.spy()};
+//
+//         weddingForm.state.name = 'foo';
+//         weddingForm.state.email = 'gn30272@gmail.com';
+//         weddingForm.onSubmit(event);
+//
+//         expect(weddingForm.state.name).toEqual('');
+//         expect(weddingForm.state.email).toEqual('');
+//       });
+//
+//       it('should not save if name evaluates to an empty string', () => {
+//         const event = {preventDefault: sinon.spy()};
+//
+//         weddingForm.state.name = '';
+//         weddingForm.state.email = '';
+//         weddingForm.onSubmit(event);
+//
+//         expect(weddingForm.props.createTask.callCount).toBe(0);
+//
+//         weddingForm.state.name = '    ';
+//         weddingForm.state.email = '';
+//         weddingForm.onSubmit(event);
+//
+//         expect(weddingForm.props.createTask.callCount).toBe(0);
+//       });
+//     });
+//   });
+//
+//
+//   describe('DOM:', () => {
+//     describe('`keyup` event triggered on text field with escape key', () => {
+//       it('should set #state.name with an empty string', () => {
+//         weddingForm.setState({name: 'foo', 'email': 'gn30272@gmail.com'});
+//         Simulate.keyUp(weddingForm.nameInput, {keyCode: 27});
+//         Simulate.keyUp(weddingForm.emailInput, {keyCode: 27});
+//         expect(weddingForm.state.name).toEqual('');
+//         expect(weddingForm.state.email).toEqual('');
+//       });
+//
+//       it('should set text field value with an empty string', () => {
+//         weddingForm.setState({name: 'foo', 'email': 'gn30272@gmail.com'});
+//         Simulate.keyUp(weddingForm.nameInput, {keyCode: 27});
+//         Simulate.keyUp(weddingForm.emailInput, {keyCode: 27});
+//         expect(weddingForm.nameInput.value).toEqual('');
+//         expect(weddingForm.emailInput.value).toEqual('');
+//       });
+//     });
+//
+//
+//     describe('`change` event triggered on text field', () => {
+//       it('should set #state.name with the value from the text field', () => {
+//         weddingForm.nameInput.value = 'foo';
+//         expect(weddingForm.state.name).toEqual('');
+//         Simulate.change(weddingForm.nameInput);
+//         expect(weddingForm.state.name).toEqual('foo');
+//       });
+//     });
+//
+//
+//     describe('`submit` event triggered on form', () => {
+//       it('should prevent the default action of the event', () => {
+//         const event = {preventDefault: sinon.spy()};
+//         Simulate.submit(findDOMNode(weddingForm), event);
+//         expect(event.preventDefault.callCount).toEqual(1);
+//       });
+//
+//       it('should set text field value with an empty string', () => {
+//         const event = {preventDefault: sinon.spy()};
+//         weddingForm.setState({name: 'foo'});
+//         Simulate.submit(findDOMNode(weddingForm), event);
+//         expect(weddingForm.nameInput.value).toEqual('');
+//       });
+//     });
+//   });
+// });
